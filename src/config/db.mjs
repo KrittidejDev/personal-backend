33689e390797a595088a582a06221178e6a5e611
KrittidejDev <krittidej.dev@gmail.com>
2025-09-06 23:08:09 +0700

    fix error vercel

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -1,14 +1,25 @@
 import "dotenv/config";
-import mongooose from "mongoose";
+import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    await mongooose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected ok");
-  } catch (err) {
-    console.log("MongoDB connection error:", err);
-    process.exit(1);
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.MONGO_URI)
+      .then((mongoose) => {
+        return mongoose;
+      });
   }
-};
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 export default connectDB;
