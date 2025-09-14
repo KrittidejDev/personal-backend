3223a54cdcdd5571042e958d1648ea4009a709c8
KrittidejDev <krittidej.dev@gmail.com>
2025-09-14 09:58:46 +0700

    edit notification

--- src/app.mjs
+++ src/app.mjs
@@ -33,7 +33,7 @@ app.use("/api/categories", categoryRoutes);
 app.use("/api/blogs", blogRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
-app.use("/api/likes", notificationRoutes);
+app.use("/api/notification", notificationRoutes);
 
 app.get("/test", (req, res) => {
   res.status(200).json({ message: "Test route works!" });
