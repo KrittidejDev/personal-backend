febe692fc7e709bfb570fe6e2ad1dcb5d58524c2
KrittidejDev <krittidej.dev@gmail.com>
2025-09-21 00:24:37 +0700

    fix like

--- src/routes/likeRoutes.mjs
+++ src/routes/likeRoutes.mjs
@@ -1,10 +1,11 @@
 import express from "express";
 import * as likeController from "../controllers/likeController.mjs";
+import { protect } from "../middlewares/authMiddleware.mjs";
 
 const router = express.Router();
 
-router.post("/like/:blogId", likeController.likeBlog);
-router.delete("/unlike/:blogId", likeController.unlikeBlog);
+router.post("/like/:blogId", protect, likeController.likeBlog);
+router.delete("/unlike/:blogId", protect, likeController.unlikeBlog);
 router.get("/:blogId", likeController.getLikesByBlog);
 
 export default router;
